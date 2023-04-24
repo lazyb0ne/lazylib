189126d95fe4b80896ea9384cddd3964b8a5ed77
lazybone <lgtwboy@sina.com>
2023-04-24 21:23:27 +0800

    fix

--- playwright/analyse_video_web/src/video_info.py
+++ playwright/analyse_video_web/src/video_info.py
@@ -6,15 +6,20 @@ import os
 import time
 import openpyxl
 import json
+
+from openpyxl.reader.excel import load_workbook
 from playwright.async_api import async_playwright
 import xlwt
 from datetime import datetime
 from playwright.sync_api import sync_playwright
+import re
+import sys
 
 
 # 20230413 config
 video_url_list = []
 video_url_list_url = '../input/视频网站整理.xlsx'
+
 
 def video_read_url():
     print(os.getcwd())
@@ -27,6 +32,7 @@ def video_read_url():
         video_url_list.append(row['首页'])
     print("video_read_url() count = " + str(len(video_url_list)))
 
+
 def clearText(text):
     # 检查参数是否为空
     if not text:
@@ -36,72 +42,104 @@ def clearText(text):
         return False
     return text
 
-# async def video_content_by_url():
+
 def video_content_by_url():
-    # xls config
-    workbook = xlwt.Workbook()
-    sheet = workbook.add_sheet('VideoUrlResult')
-    sheet.set_panes_frozen(1)  # 冻结行数
-    sheet.set_horz_split_pos(1)  # 设置水平分割位置，1表示第1行之上
-    sheet.write(0, 0, 'Web - All - Kind')
-    sheet.write(0, 1, 'FROM')
-    sheet.write(0, 2, 'Web/Mobile')
-    sheet.write(0, 3, 'Kind:text/external_url/internal_url/text_mobile/external_url_mobile/internal_url_mobile')
-    sheet.write(0, 4, 'Content')
-    sheet.col(0).width = 256 * 20
-    sheet.col(1).width = 256 * 20
-    sheet.col(3).width = 256 * 20
-    sheet.col(4).width = 256 * 100
-    cur_time = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
-    search_results_path = f'../output/VideoUrlResult{cur_time}.xls'
-    xls_row = 1
-    new_url = 1
-    web_index = 1
-    kind_index = 1
-    toWrite = []
-    for video_url in video_url_list:
-        new_url = 1
+    start_time = time.time()
+    # cur_time = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+    cur_time = datetime.now().strftime("%Y-%m-%d")
+    # search_results_path = f'../output/VideoUrlResult{cur_time}.xlsx'
+    search_results_path = f'../output/VideoUrlResult{cur_time}.xlsx'
+    # openpyxl config
+    # workbook = openpyxl.Workbook()
+    try:
+        workbook = load_workbook(search_results_path)
+    except:
+        workbook = openpyxl.Workbook()
+        workbook.save(search_results_path)
+
+    worksheet = workbook.active
+    worksheet['A1'] = 'Web - All - Kind'
+    worksheet['B1'] = 'FROM'
+    worksheet['C1'] = 'Web/Mobile'
+    worksheet['D1'] = 'Kind:text/external_url/internal_url/text_mobile/external_url_mobile/internal_url_mobile'
+    worksheet['E1'] = 'Content'
+    worksheet.column_dimensions['A'].width = 15
+    worksheet.column_dimensions['B'].width = 40
+    worksheet.column_dimensions['C'].width = 15
+    worksheet.column_dimensions['D'].width = 30
+    worksheet.column_dimensions['E'].width = 100
+    worksheet.freeze_panes = 'A2'
+    video_url_list_count = len(video_url_list)
+    url_ok = get_web_url(search_results_path)
+    print(url_ok)
+    print(f"Already get: {len(url_ok)}")
+    xls_row_count = 0
+    for video_idx, video_url in enumerate(video_url_list):
         # video_url = 'http://www.nongkenfang.com' # 测试站点
+        print(f"Current url:{video_url}")
+        is_continue = False
+        if video_url in url_ok:
+            try:
+                # 最后一条删掉重新跑
+                if video_url == url_ok[-1]:
+                    print(f"Last need to delete:   {video_url}")
+                    word_in_xlsx(search_results_path, video_url)
+                    print(f"Deleted OK:   {video_url}")
+                else:
+                    print(f"continue   {video_url}")
+                    is_continue = True
+                    continue
+            except:
+                print("")
+        if is_continue:
+            continue
         try:
             page_text, internal_links, external_links = download_website_text_and_links(video_url, False)
-            page_text_mobile, internal_links_mobile, external_links_mobile = download_website_text_and_links(video_url, False)
+            page_text_mobile, internal_links_mobile, external_links_mobile = download_website_text_and_links(video_url, True)
         except:
             continue
-
-        list_all = [page_text, internal_links, external_links, page_text_mobile, internal_links_mobile, external_links_mobile]
+        list_all = [page_text, internal_links, external_links, page_text_mobile, internal_links_mobile,
+                    external_links_mobile]
         list_web_mobile = ['web', 'web', 'web', 'mobile', 'mobile', 'mobile']
-        list_kink = ['text', 'internal_links', 'external_links', 'text_mobi', 'internal_links_mobi', 'external_links_mobi']
-        for i in range(6):
+        list_kink = ['text', 'internal_links', 'external_links', 'text_mobi', 'internal_links_mobi',
+                     'external_links_mobi']
+
+        for i in range(len(list_all)):
+            url_cost = time.time()
             kind_index = 0
             for item in list_all[i]:
+                kind_cost = time.time()
                 item = clearText(item)
                 if item is False:
                     continue
                 kind_index += 1
-                if list_kink[i] in ['internal_links', 'internal_links_mobi']:
-                    toWrite.append([web_index, video_url, list_web_mobile[i], list_kink[i], video_url + item, kind_index])
-                else:
-                    toWrite.append([web_index, video_url, list_web_mobile[i], list_kink[i], item, kind_index])
+                first_data = str(video_idx) + " - " + str(xls_row_count) + " - " + str(kind_index)
+                # print(first_data)
+                if list_kink[i] in ['internal_links', 'internal_links_mobi'] and video_url not in item:
+                    item = video_url + item
+                try:
+                    worksheet.append([first_data, video_url, list_web_mobile[i], list_kink[i], item])
+                    xls_row_count += 1
+                    show_info(video_idx, xls_row_count, kind_index, start_time, video_url_list_count, url_cost,
+                              kind_cost)
+                except Exception:
+                    pass
 
-        if toWrite:
-            for item in toWrite:
-                sheet.write(xls_row, 0, str(item[0]) + " - " + str(xls_row) + " - " + str(item[5]))
-                sheet.write(xls_row, 1, item[1])
-                sheet.write(xls_row, 2, item[2])
-                sheet.write(xls_row, 3, item[3])
-                sheet.write(xls_row, 4, item[4])
-                xls_row += 1
-            web_index += 1
-
-        print(f"Current Index: {web_index}")
-        # if web_index > 5:
-        #     break
-
-    if search_results_path:
+        if video_idx > 300:
+            break
         workbook.save(search_results_path)
 
+
+def show_info(video_idx, xls_row_count, kind_index, start_time, video_url_list_count, url_cost, kind_cost):
+    t_all = time.time() - start_time
+    t_url = time.time() - url_cost
+    t_kind = time.time() - kind_cost
+    print("{} / {} kind: {} cost(sec) ALL/URL/Kind:{:.2f} / {:.2f} / {:.2f}".format(video_url_list_count, video_idx, kind_index, t_all, t_url, t_kind))
+    # print(f"video:{video_idx}")
+
+
 # 下载某个url所有的标签、文本
-def download_website_text_and_links(url, isPhone = False, headless = True):
+def download_website_text_and_links(url, isPhone=False, headless=True):
     with sync_playwright() as playwright:
         if isPhone:
             iphone = playwright.devices['iPhone 12 Pro Max']
@@ -135,10 +173,98 @@ def download_website_text_and_links(url, isPhone = False, headless = True):
 
     return unique_list, internal_links, external_links
 
+
+def lazy():
+    cur_time = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+    search_results_path = f'../output/VideoUrlResult{cur_time}.xlsx'
+
+    workbook = openpyxl.Workbook()
+    worksheet = workbook.active
+    worksheet['A1'] = 'Web - All - Kind'
+    worksheet['B1'] = 'FROM'
+    worksheet['C1'] = 'Web/Mobile'
+    worksheet['D1'] = 'Kind:text/external_url/internal_url/text_mobile/external_url_mobile/internal_url_mobile'
+    worksheet['E1'] = 'Content'
+    # 写入数据到多个单元格
+    data = [
+        ['Name', 'Age', 'City'],
+        ['Alice', 25, 'New York'],
+        ['Bob', 30, 'Los Angeles'],
+        ['Charlie', 35, 'Chicago']
+    ]
+    for row in data:
+        worksheet.append(row)
+        # 保存 Excel 文件
+    workbook.save('example.xlsx')
+
+
+def is_valid_url(url):
+    pattern = re.compile(
+        r'^(?:http|ftp)s?://'  # scheme
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?))'  # domain
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+    return bool(re.match(pattern, url))
+
+def get_last_row_data(xlsx_file):
+    workbook = openpyxl.load_workbook(xlsx_file)
+    sheet = workbook.active
+    last_row = sheet.max_row
+    last_row_values = []
+    for column in sheet.iter_cols(min_row=last_row, max_row=last_row, values_only=True):
+        last_row_values = column
+    last_row_values = [cell.value for cell in sheet[last_row]]
+    return last_row_values
+
+
+def get_web_url(xlsx_file):
+    workbook = openpyxl.load_workbook(xlsx_file)
+    sheet = workbook.active
+    web_url_ok = []
+    for row in sheet.iter_rows():
+        web_url_ok.append(row[1].value)
+    return np.unique(web_url_ok)
+
+def word_in_xlsx(file, str_to_delete):
+    workbook = openpyxl.load_workbook(file)
+    sheet = workbook.active
+    word_found = False
+    rows_to_delete = []
+
+    # Iterate through the rows and check if the word is present in the row data
+    for row in range(1, sheet.max_row + 1):
+        print("--------")
+        print(row)
+        # row_data = [cell.value for cell in sheet[row]]  # Get the values of cells in the row
+        row_data = [sheet[row][1].value]  # Get the values of cells in the row
+        print(row_data)
+
+        if str_to_delete in row_data:
+            rows_to_delete.append(row)  # Add the row number to the list of rows to delete
+
+    # Delete the rows in reverse order to avoid issues with row deletion
+    idx = 1
+    for row in reversed(rows_to_delete):
+        sheet.delete_rows(row)  # Delete the row
+        print(f"{idx} Deleted: {str_to_delete}")
+        idx +=1
+
+    # Save the updated Excel file
+    workbook.save(file)  # Replace 'file_updated.xlsx' with the desired name for the updated file
+
+def test():
+    file = "../output/VideoUrlResult230424.xlsx"
+    # web_list = get_last_row_data(file)
+    web_list = get_web_url(file)
+    print(web_list)
+    # word_in_xlsx(file, "https://fua.cc/")
+    # print()
+    # for idx, row in enumerate([11,22,33]):
+    #     print(f"Deleted: {idx}")
+
+
 if __name__ == '__main__':
     video_read_url()
     video_content_by_url()
-
-
-
+    # lazy()
+    # test()
 
